c84fbd8d8196f4d63e1c14302e2cceac888afe5d
tong <toniguan@gmail.com>
2018-10-16 14:54:34 -0700

    Updates

---
+++ precache-manifest.f01aeb02b1b9ddc23da5a03ee06a5dfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4f46611e166d396c4e4",
+    "url": "/neighbourhood-map/static/js/runtime~main.b4f46611.js"
+  },
+  {
+    "revision": "33edfb1909a497a464c0",
+    "url": "/neighbourhood-map/static/js/main.33edfb19.chunk.js"
+  },
+  {
+    "revision": "b447446b792683654e4d",
+    "url": "/neighbourhood-map/static/js/1.b447446b.chunk.js"
+  },
+  {
+    "revision": "33edfb1909a497a464c0",
+    "url": "/neighbourhood-map/static/css/main.7f53ea5e.chunk.css"
+  },
+  {
+    "revision": "da7f4fee28eef0aaa45843975a5ce588",
+    "url": "/neighbourhood-map/index.html"
+  }
+];
